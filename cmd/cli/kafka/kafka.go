4815e22be241195d4e1fa59acf6e476d07173b11
your name <lathanhmta@gmail.com>
2026-02-15 21:51:38 +0700

    Add kafka

---
+++ cmd/cli/kafka/kafka.go
@@ -0,0 +1,31 @@
+package kafka
+
+import (
+	"strings"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+// for consumer
+func getKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+	return &kafka.Writer{
+		Addr:     kafka.TCP(kafkaURL),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+}
+
+// for producer
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:        brokers,
+		GroupID:        groupID,
+		Topic:          topic,
+		MinBytes:       10e3, // 10KB
+		MaxBytes:       10e6, // 10MB
+		CommitInterval: time.Second,
+		StartOffset:    kafka.LastOffset,
+	})
+}
